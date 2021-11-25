c33fd40bcca17c1ad1dfc114fbc7bb45979e224a
Progress Aienobe <paienobe@gmail.com>
2021-11-25 20:55:03 +0100

    Wrote the js code over again from scratch
    Everything seems to be working now

--- main.js
+++ main.js
@@ -1,101 +1,180 @@
 const darkMode = document.getElementById("to-dark")
 const lightMode = document.getElementById("to-light")
 const header = document.querySelector(".header")
+const insertSection = document.querySelector(".insert-section")
 const insertBox = document.querySelector("#insert-box")
 const itemHouse = document.querySelector(".item-housing")
+const container = document.querySelector(".item-container")
+const tabs = document.querySelector(".tabs")
+let completed = []
+
+
+// switching betwwwn light and dark mode
+darkMode.addEventListener("click", () => {
+    darkMode.style.display = "none"
+    lightMode.style.display = "block"
+    document.body.style.backgroundColor = "hsl(235, 21%, 11%)"
+    insertSection.style.backgroundColor = "hsl(235, 24%, 19%)"
+    insertBox.style.backgroundColor = "hsl(235, 24%, 19%)"
+    container.style.backgroundColor = "hsl(235, 24%, 19%)"
+    tabs.style.backgroundColor = "hsl(235, 24%, 19%)"
+    if (window.innerWidth < 950) {
+        header.style.backgroundImage = `url("./images/bg-mobile-dark.jpg")`
+    } else {
+        header.style.backgroundImage = `url("./images/bg-desktop-dark.jpg")`
+    }
+})
+
+
+lightMode.addEventListener("click", () => {
+    lightMode.style.display = "none"
+    darkMode.style.display = "inline"
+    document.body.style.backgroundColor = "hsl(236, 33%, 92%)"
+    insertSection.style.backgroundColor = "white"
+    insertBox.style.backgroundColor = "white"
+    container.style.backgroundColor = "white"
+    tabs.style.backgroundColor = "white"
+    if (window.innerWidth < 950) {
+        header.style.backgroundImage = `url("./images/bg-mobile-light.jpg")`
+    } else {
+        header.style.backgroundImage = `url("./images/bg-desktop-light.jpg")`
+    }
+})
+
+
+// adding items to list
 let count = 0
 const countRecord = document.querySelector(".count-record")
 countRecord.innerHTML = `${count} item(s) left`
+let circle = []
+let delBtn = []
+let todoItems = []
 let item = []
 let allActive = []
-let completed = []
-const clearBtn = document.querySelector(".clear")
-let itemText = []
-
-const valueHolder = []
 
 insertBox.addEventListener("keypress", (e) => {
-    if (e.key === "Enter") {
-        if (insertBox.value !== "") {
-            itemHouse.innerHTML += `
-            <div class="item active-item">
-                <div class="item-content">
-                    <div class="check"></div>
-                    <p class="item-text">${insertBox.value}</p>
-                </div>
-                <button class="close"></button>
-            </div>
-        `
-        count++
-        countRecord.innerHTML = `${count} item(s) left`
-        item = document.querySelectorAll(".item")
-        allActive = document.querySelectorAll(".active-item")
-        itemText = document.querySelectorAll(".item-text")
-        // console.log(itemHouse)
-        valueHolder.push({value: insertBox.value})
-        localStorage.setItem("myData", JSON.stringify(valueHolder))
-        }
-    insertBox.value = ""
+    const todo = {
+        text: "",
+        checked: false,
+        id: Date.now()
     }
-    const circle = document.querySelectorAll(".check")
+    if (insertBox.value !== "") {
+        itemHouse.innerHTML += `
+        <div class="item active-item">
+            <div class="item-content">
+                <div class="check"></div>
+                <p class="item-text" id="${todo.id}">${insertBox.value}</p>
+            </div>
+            <button class="close"></button>
+        </div>
+    `
+        todo.text = insertBox.value
+        todoItems.push(todo)
+        insertBox.value = ""
+        allActive = document.querySelectorAll(".active-item")
+        circle = document.querySelectorAll(".check")
+        delBtn = document.querySelectorAll(".close")
+        count = allActive.length
+        countRecord.innerHTML = `${count} item(s) left`
+        setLocalStorage()
+    }
+
     circle.forEach((circle) => {
         circle.addEventListener("click", () => {
             circle.classList.toggle("clicked")
-            if (circle.classList.contains("clicked")) {
-                circle.parentElement.childNodes[3].style.textDecoration = "line-through"
-                circle.parentElement.childNodes[3].style.color = "hsl(236, 9%, 61%)"
-                count--
-                countRecord.innerHTML = `${count} item(s) left`
-                circle.parentElement.parentElement.classList.add("completed")
-                circle.parentElement.parentElement.classList.remove("active-item")
-                completed = document.querySelectorAll(".completed")
-            } else {
-                circle.parentElement.childNodes[3].style.textDecoration = "none"
-                circle.parentElement.childNodes[3].style.color = "hsl(235, 19%, 35%)"
-                count++
-                countRecord.innerHTML = `${count} item(s) left`
-                circle.parentElement.parentElement.classList.remove("completed")
-                circle.parentElement.parentElement.classList.add("active-item")
-            }
-        }) 
+            checkedItem(circle)
+        })
     })
 
-    const delBtn = document.querySelectorAll(".close")
     delBtn.forEach((btn) => {
         btn.addEventListener("click", () => {
-            let indexParent = Array.from(allActive)
-            let index = indexParent.indexOf(btn.parentElement)
-            console.log(index)
-            if (valueHolder[index] !== -1) {
-                    valueHolder.splice(index, 1)
-                    console.log(valueHolder)
-                    console.log(indexParent)
-                    localStorage.setItem("myData", JSON.stringify(valueHolder))
-            }
-            if (!btn.parentElement.children[0].children[0].classList.contains("clicked")) {
-                btn.parentElement.remove()
-                count--
-                countRecord.innerHTML = `${count} item(s) left`
-            } else {
-                btn.parentElement.remove()
-                countRecord.innerHTML = `${count} item(s) left`
-            }
+            deleteBtn(btn)
         })
     })
 })
 
-const tabs = document.querySelector(".tabs")
-const allTab = document.getElementById("all")
-const activeTab = document.getElementById("active")
-const completeTab = document.getElementById("complete")
+
+function checkedItem(par) {
+            if (par.classList.contains("clicked")) {
+                par.parentElement.childNodes[3].style.textDecoration = "line-through"
+                par.parentElement.childNodes[3].style.color = "hsl(236, 9%, 61%)"
+                count--
+                countRecord.innerHTML = `${count} item(s) left`
+                par.parentElement.parentElement.classList.add("completed")
+                par.parentElement.parentElement.classList.remove("active-item")
+                completed = document.querySelectorAll(".completed")
+                for (let i = 0; i < todoItems.length; i++) {
+                    if (todoItems[i].id === Number(par.parentElement.children[1].id)) {
+                        todoItems[i].checked = true
+                    } 
+                }
+            } else {
+                par.parentElement.childNodes[3].style.textDecoration = "none"
+                par.parentElement.childNodes[3].style.color = "hsl(235, 19%, 35%)"
+                count++
+                countRecord.innerHTML = `${count} item(s) left`
+                par.parentElement.parentElement.classList.remove("completed")
+                par.parentElement.parentElement.classList.add("active-item")
+                for (let i = 0; i < todoItems.length; i++) {
+                    if (todoItems[i].id === Number(par.parentElement.children[1].id)) {
+                        todoItems[i].checked = false
+                    }
+                }
+            }        
+}
 
 
-document.addEventListener("DOMContentLoaded", () => {
-    allTab.style.color = "hsl(220, 98%, 61%)"
+function deleteBtn(par2) {
+        let itemID = Number(par2.parentElement.children[0].children[1].id) 
+        let idFilter = todoItems.filter((object) => {
+            if(object.id !== itemID) {
+                return true
+            }
+        })
+        todoItems = idFilter
+
+        if (!par2.parentElement.children[0].children[0].classList.contains("clicked")) {
+            par2.parentElement.remove()
+            allActive = document.querySelectorAll(".active-item")
+            count = Number(allActive.length)
+            countRecord.innerHTML = `${count} item(s) left`
+        } else {
+            par2.parentElement.remove()
+            allActive = document.querySelectorAll(".active-item")
+            count = Number(allActive.length)
+            countRecord.innerHTML = `${count} item(s) left`
+        }
+
+        setLocalStorage()
+}
+
+
+// adding items to local storage
+function setLocalStorage() {
+    localStorage.setItem("myData", JSON.stringify(todoItems))
+}
+
+
+// clear button
+const clearBtn = document.querySelector(".clear")
+clearBtn.addEventListener ("click", () => {
+    const removeCleared = todoItems.filter((object) => {
+        if (object.checked !== true) {
+            return true
+        }
+    })
+    todoItems = removeCleared
+    completed.forEach((task) => {
+        task.remove()
+    })
+    setLocalStorage()
 })
 
 
-let activeFilter = []
+// tab buttons
+const allTab = document.getElementById("all")
+const activeTab = document.getElementById("active")
+const completeTab = document.getElementById("complete")
 
 activeTab.addEventListener("click", () => {
     allTab.style.color = "hsl(235, 19%, 35%)"
@@ -139,73 +218,69 @@ completeTab.addEventListener("click", () => {
     })
 })
 
-clearBtn.addEventListener ("click", () => {
-    completed.forEach((task) => {
-        task.remove()
-    })
-})
 
-const container = document.querySelector(".item-container")
-const insertSection = document.querySelector(".insert-section")
-let light = []
+// get items from local storage
+let storedData = localStorage.getItem("myData")
+let parsedDataArray = JSON.parse(storedData)
 
-darkMode.addEventListener ("click", () => {
-    darkMode.style.display = "none"
-    lightMode.style.display = "inline"
-    document.body.style.backgroundColor = "hsl(235, 21%, 11%)"
-    insertSection.style.backgroundColor = "hsl(235, 24%, 19%)"
-    insertBox.style.backgroundColor = "hsl(235, 24%, 19%)"
-    container.style.backgroundColor = "hsl(235, 24%, 19%)"
-    tabs.style.backgroundColor = "hsl(235, 24%, 19%)"
-    if (window.innerWidth < 950) {
-        header.style.backgroundImage = `url("./images/bg-mobile-dark.jpg")`
-    } else {
-        header.style.backgroundImage = `url("./images/bg-desktop-dark.jpg")`
-    }
-})
-
-lightMode.addEventListener("click", () => {
-    lightMode.style.display = "none"
-    darkMode.style.display = "inline"
-    document.body.style.backgroundColor = "hsl(236, 33%, 92%)"
-    insertSection.style.backgroundColor = "white"
-    insertBox.style.backgroundColor = "white"
-    container.style.backgroundColor = "white"
-    tabs.style.backgroundColor = "white"
-    if (window.innerWidth < 950) {
-        header.style.backgroundImage = `url("./images/bg-mobile-light.jpg")`
-    } else {
-        header.style.backgroundImage = `url("./images/bg-desktop-light.jpg")`
-    }
-})
 
 document.addEventListener("DOMContentLoaded", () => {
-    // let storedData = localStorage.getItem("myData")
-    // let parsedDataArray = JSON.parse(storedData)
-    // for (let i = 0; i < parsedDataArray.length; i++) {
-    //     itemHouse.innerHTML += `
-    //     <div class="item active-item">
-    //         <div class="item-content">
-    //             <div class="check"></div>
-    //             <p class="item-text">${parsedDataArray[i].value}</p>
-    //         </div>
-    //         <button class="close"></button>
-    //     </div>
-    // `
-    // count++
-    // countRecord.innerHTML = `${count} item(s) left`
-    // }
-    // const delBtn = document.querySelectorAll(".close")
-    // delBtn.forEach((btn) => {
-    //     btn.addEventListener("click", () => {
-    //         if (!btn.parentElement.children[0].children[0].classList.contains("clicked")) {
-    //             btn.parentElement.remove()
-    //             count--
-    //             countRecord.innerHTML = `${count} item(s) left`
-    //         } else {
-    //             btn.parentElement.remove()
-    //             countRecord.innerHTML = `${count} item(s) left`
-    //         }
-    //     })
-    // })
+    allTab.style.color = "hsl(220, 98%, 61%)"
+
+    for (let i = 0; i < parsedDataArray.length; i++) {
+        todoItems.push(parsedDataArray[i])
+    }
+
+
+    for (let j = 0; j < parsedDataArray.length; j++) {
+        if (todoItems[j].checked === true) {
+            itemHouse.innerHTML += `
+                    <div class="item completed">
+                        <div class="item-content">
+                            <div class="check clicked"></div>
+                            <p class="item-text" id="${parsedDataArray[j].id}">${parsedDataArray[j].text}</p>
+                        </div>
+                        <button class="close"></button>
+                    </div>
+                `
+        } else {
+            itemHouse.innerHTML += `
+            <div class="item active-item">
+                <div class="item-content">
+                    <div class="check"></div>
+                    <p class="item-text" id="${parsedDataArray[j].id}">${parsedDataArray[j].text}</p>
+                </div>
+                <button class="close"></button>
+            </div>
+        `
+        }
+
+        
+        circle = document.querySelectorAll(".check")
+        delBtn = document.querySelectorAll(".close")
+        allActive = document.querySelectorAll(".active-item")
+        count = allActive.length
+        countRecord.innerHTML = `${count} item(s) left`
+
+
+        circle.forEach((circle) => {
+            if (circle.classList.contains("clicked")) {
+                checkedItem(circle)
+                count = allActive.length
+                countRecord.innerHTML = `${count} item(s) left`
+            }
+
+            circle.addEventListener("click", () => {
+                circle.classList.toggle("clicked")
+                checkedItem(circle)
+                setLocalStorage()
+            })
+        })
+
+        delBtn.forEach((btn) => {
+            btn.addEventListener("click", () => {
+                deleteBtn(btn)
+            })
+        })
+        }
 })
